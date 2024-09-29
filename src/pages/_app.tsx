ffbf3d641f1e6bdc05be37d117a86698223894a3
José Vitor <jose-vitor-7@hotmail.com>
2024-09-29 20:43:59 -0300

    feat: setup theme toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 App.displayName = 'App'
