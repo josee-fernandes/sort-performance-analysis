4c60b9eb4d59f84abf852286a484059b263b71c6
José Vitor <jose-vitor-7@hotmail.com>
2024-10-22 20:09:10 -0300

    fix: use env vars for api base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const placeholderApi = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com',
+  baseURL: process.env.NEXT_PUBLIC_PLACEHOLDER_API_BASE_URL,
 })
 
 export const appApi = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${process.env.NEXT_PUBLIC_APP_URL}/api`,
 })
